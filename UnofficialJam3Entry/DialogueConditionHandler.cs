d244459e9a7d677491c56752af1ff3a5ade9896e
Nick <nicholasconnors@hotmail.ca>
2024-03-22 20:58:15 -0400

    Most of the planet done

---
+++ UnofficialJam3Entry/DialogueConditionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnofficialJam3Entry;
+
+internal static class DialogueConditionHandler
+{
+    public static void Setup()
+    {
+        GlobalMessenger.AddListener("ExitConversation", OnExitConversation);
+    }
+
+    private static void OnExitConversation()
+    {
+        if (DialogueConditionManager.SharedInstance.GetConditionState("SecretRecordingKaboom"))
+        {
+            GameObject.Find("Gravelrock_Body/Sector/SecretRecording/Explosion").GetComponent<ExplosionController>().Play();
+        }
+    }
+}
